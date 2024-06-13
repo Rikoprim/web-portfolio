aa3816e2188ec805eb1017d3f286cb792886d9a5
Rikoprim <rikoprimada@gmail.com>
2024-06-13 18:39:53 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Button from 'react-bootstrap/Button';
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Button as="a" variant="primary">
+        Button as link
+      </Button>
+    </>
+  )
+}
+
+export default App
